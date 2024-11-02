986b3bf5ece13290484692e09befa3faf4a2306a
Muhammad Yahya Muhaimin <surat.pribadiku@gmail.com>
2024-11-03 05:12:09 +0700

    feat: Users model

---
+++ internal/db/models/users.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+)
+
+type Users struct {
+	ID            int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeactivatedAt time.Time
+	Email         string
+	Secret        string
+}

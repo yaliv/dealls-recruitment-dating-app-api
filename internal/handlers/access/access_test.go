a9c87d5c54eebbfa702e3455ff7608c4610bbcb2
Muhammad Yahya Muhaimin <surat.pribadiku@gmail.com>
2024-11-03 21:29:55 +0700

    test: access.Login

---
+++ internal/handlers/access/access_test.go
@@ -0,0 +1,56 @@
+package access_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/buger/jsonparser"
+	"github.com/gofiber/fiber/v2"
+
+	"yaliv/dating-app-api/internal/handlers/access"
+	"yaliv/dating-app-api/internal/handlers/access/accessform"
+	"yaliv/dating-app-api/internal/helpers/testinghelper"
+)
+
+func TestLogin(t *testing.T) {
+	testinghelper.CompleteSetup(t)
+
+	app := fiber.New()
+
+	app.Post("/", accessform.ParseLogin, access.Login)
+
+	reqBody := map[string]any{
+		"email":    "MimosaBurrows@jourrapide.com",
+		"password": "Husoh0EeP",
+	}
+	reqBodyJson := new(bytes.Buffer)
+	json.NewEncoder(reqBodyJson).Encode(reqBody)
+
+	req := httptest.NewRequest("POST", "/", reqBodyJson)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, _ := app.Test(req)
+	defer res.Body.Close()
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testinghelper.CheckHttpStatus(t, res.StatusCode, 200)
+	testinghelper.CheckSuccess(t, resBody)
+
+	testinghelper.CheckData(t, resBody, testinghelper.DataTests{
+		"access_token": testinghelper.PropertyTest{Type: jsonparser.Object, Value: nil},
+		"id":           testinghelper.PropertyTest{Type: jsonparser.Number, Value: "1"},
+		"email":        testinghelper.PropertyTest{Type: jsonparser.String, Value: reqBody["email"]},
+	})
+
+	testinghelper.CheckData(t, resBody, testinghelper.DataTests{
+		"value":      testinghelper.PropertyTest{Type: jsonparser.String, Value: nil},
+		"expired_at": testinghelper.PropertyTest{Type: jsonparser.String, Value: nil},
+	}, "data", "access_token")
+}
